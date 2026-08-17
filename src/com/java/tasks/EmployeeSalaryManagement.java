bede42f28e10c19bc439002e23d846980eb628de
osai588 <osaimurari@gmail.com>
2026-08-17 10:34:35 +0530

    Employee Salary Management System

---
+++ src/com/java/tasks/EmployeeSalaryManagement.java
@@ -0,0 +1,162 @@
+package com.java.tasks;
+
+import java.util.Scanner;
+
+class Employee {
+	private int employeeId;
+	private String employeeName;
+	private double basicSalary;
+
+	Employee(double basicSalary, int employeeId, String employeeName) {
+		this.basicSalary = basicSalary;
+		this.employeeId = employeeId;
+		this.employeeName = employeeName;
+	}
+
+	public void setSalary(double basicSalary) {
+		this.basicSalary = basicSalary;
+	}
+
+	public double getSalary() {
+		return basicSalary;
+	}
+
+	public void setEmployeeId(int employeeId) {
+		this.employeeId = employeeId;
+	}
+
+	public int getEmployeeId() {
+		return employeeId;
+	}
+
+	public void setEmployeeName(String employeeName) {
+		this.employeeName = employeeName;
+	}
+
+	public String getEmployeeName() {
+		return employeeName;
+	}
+
+	public void calculateSalary() {
+
+	}
+
+	public void displayDetails() {
+
+	}
+
+}
+
+class Developer extends Employee {
+	Developer(double basicSalary, int employeeId, String employeeName) {
+		super(basicSalary, employeeId, employeeName);
+
+	}
+
+	double allowances;
+
+	@Override
+	public void calculateSalary() {
+		allowances = getSalary() * 0.2;
+	}
+
+	@Override
+	public void displayDetails() {
+		System.out.println("Employee Name : " + getEmployeeName());
+		System.out.println("Role : Devloper");
+		System.out.println("Basic Salary : " + getSalary());
+		System.out.println("Allowances : " + allowances);
+		System.out.println("Final Salary : " + (allowances + getSalary()));
+
+	}
+}
+
+class Tester extends Employee {
+
+	Tester(double basicSalary, int employeeId, String employeeName) {
+		super(basicSalary, employeeId, employeeName);
+	}
+
+	double allowances;
+
+	@Override
+	public void calculateSalary() {
+		allowances = getSalary() * 0.15;
+	}
+
+	@Override
+	public void displayDetails() {
+		System.out.println("Employee Name : " + getEmployeeName());
+		System.out.println("Role : Tester");
+		System.out.println("Basic Salary : " + getSalary());
+		System.out.println("Allowances : " + allowances);
+		System.out.println("Final Salary : " + (allowances + getSalary()));
+
+	}
+
+}
+
+class Manager extends Employee {
+
+	Manager(double basicSalary, int employeeId, String employeeName) {
+		super(basicSalary, employeeId, employeeName);
+	}
+
+	double allowances;
+
+	@Override
+	public void calculateSalary() {
+		allowances = getSalary() * 0.3;
+	}
+
+	@Override
+	public void displayDetails() {
+		System.out.println("Employee Name : " + getEmployeeName());
+		System.out.println("Role : Manager");
+		System.out.println("Basic Salary : " + getSalary());
+		System.out.println("Allowances : " + allowances);
+		System.out.println("Final Salary : " + (allowances + getSalary()));
+
+	}
+
+}
+
+public class EmployeeSalaryManagement {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter BasicSalary");
+		double basicSalary = sc.nextDouble();
+
+		System.out.println("Enter employeeId");
+		int empployeeId = sc.nextInt();
+
+		sc.nextLine();
+
+		System.out.println("Enter employee Name");
+		String employeeName = sc.nextLine();
+
+		Employee emp;
+
+		emp = new Developer(basicSalary, empployeeId, employeeName);
+		emp.calculateSalary();
+		emp.displayDetails();
+
+		System.out.println();
+
+		emp = new Tester(basicSalary, empployeeId, employeeName);
+		emp.calculateSalary();
+		emp.displayDetails();
+
+		System.out.println();
+
+		emp = new Manager(basicSalary, empployeeId, employeeName);
+		emp.calculateSalary();
+		emp.displayDetails();
+
+		sc.close();
+
+	}
+
+}
